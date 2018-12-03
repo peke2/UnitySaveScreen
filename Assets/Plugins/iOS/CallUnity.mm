8c30e337c77346ac671a7819047396f7b0e6ef23
peke2 <abem@dc4.so-net.ne.jp>
2018-12-03 12:36:01 +0900

    iOSのプラグインからUnityにメッセージを送る処理の確認。

---
+++ Assets/Plugins/iOS/CallUnity.mm
@@ -0,0 +1,8 @@
+﻿#import <Foundation/Foundation.h>
+
+extern "C" void CallUnity(const char *cbGameObjectName, const char *cbMethodName)
+{
+    NSLog(@"---- Unity側の呼び出し開始");
+    UnitySendMessage(cbGameObjectName, cbMethodName, "Call from native");
+    NSLog(@"---- Unity側の呼び出し終了");
+}
---
+++ Assets/Scripts/ReceiveCallback.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class ReceiveCallback : MonoBehaviour {
+#if UNITY_IOS
+    [DllImport("__Internal")]
+    private static extern void CallUnity(string cbGameObjectName, string cbMethodName);
+#endif
+
+    // Use this for initialization
+    void Start () {
+#if UNITY_IOS
+        CallUnity("ReceiveCallback", "called");
+#endif
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void called()
+    {
+        Debug.Log("プラグインからのコールバック");
+    }
+}
